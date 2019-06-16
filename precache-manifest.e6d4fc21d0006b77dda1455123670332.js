cf969701f8b302e5144101002833762d2babf8d9
max <max@local>
2019-06-16 19:00:59 +0500

    Updates

---
+++ precache-manifest.e6d4fc21d0006b77dda1455123670332.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d226cdd8fb4d548387f003872666d4c",
+    "url": "/react-mobx-chartjs/index.html"
+  },
+  {
+    "revision": "22e1be58529259795a49",
+    "url": "/react-mobx-chartjs/static/js/2.82773b0e.chunk.js"
+  },
+  {
+    "revision": "d6d9515ec81abe12b5bb",
+    "url": "/react-mobx-chartjs/static/js/main.6a2d2097.chunk.js"
+  },
+  {
+    "revision": "81710fb144b3304e8a7b",
+    "url": "/react-mobx-chartjs/static/js/runtime~main.a7e317c0.js"
+  }
+]);
